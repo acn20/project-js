f2f878cab65286f52bc70e75057a15cfa0515c93
Andreea <andreea.nisipeanu@gmail.com>
2019-01-11 23:54:39 +0200

    very small changes

--- scripts/proiect_js_form.js
+++ scripts/proiect_js_form.js
@@ -1,14 +1,6 @@
 var x = document.getElementsByClassName('field')[0];
 var y = document.getElementsByClassName('field')[1];
 
-function pass() {
-    if (y.value.length < 3) {
-        document.getElementsByTagName('p')[2].innerHTML = 'Minim 3 caractere';
-    } else {
-        document.getElementsByTagName('p')[2].innerHTML = '';
-    }
-}
-
 function mail() {
     if (
         x.value.indexOf('@') != x.value.lastIndexOf('@') ||
@@ -17,8 +9,16 @@ function mail() {
         x.value.lastIndexOf('.') <= x.value.indexOf('@') + 3
 
     ) {
-        document.getElementsByTagName('p')[1].innerHTML = 'E-mail incorect';
+        document.getElementsByTagName('span')[0].innerHTML = 'E-mail incorect';
     } else {
-        document.getElementsByTagName('p')[1].innerHTML = '';
+        document.getElementsByTagName('span')[0].innerHTML = '';
+    }
+}
+
+function pass() {
+    if (y.value.length < 3) {
+        document.getElementsByTagName('span')[1].innerHTML = 'Minim 3 caractere';
+    } else {
+        document.getElementsByTagName('span')[1].innerHTML = '';
     }
 }
