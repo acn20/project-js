5a296c7b37dd67669267c2e35471e4489fd1d977
Andreea <andreea.nisipeanu@gmail.com>
2019-01-13 23:28:14 +0200

    finished

--- scripts/proiect_js_form.js
+++ scripts/proiect_js_form.js
@@ -6,19 +6,23 @@ function mail() {
         x.value.indexOf('@') != x.value.lastIndexOf('@') ||
         x.value.indexOf('@') < 2 ||
         x.value.lastIndexOf('.') > x.value.length - 3 ||
-        x.value.lastIndexOf('.') <= x.value.indexOf('@') + 3
+        x.value.lastIndexOf('.') <= x.value.indexOf('@') + 2
 
     ) {
-        document.getElementsByTagName('span')[0].innerHTML = 'Incorrect email address';
+        document.getElementsByClassName('logInMessage')[0].innerHTML = 'Incorrect email address';
+        x.setAttribute("style", "border: 1px solid #FF8A80");
     } else {
-        document.getElementsByTagName('span')[0].innerHTML = '';
+        document.getElementsByClassName('logInMessage')[0].innerHTML = '';
+        x.setAttribute("style", "border: 1px solid #ddd");
     }
 }
 
 function pass() {
     if (y.value.length < 3) {
-        document.getElementsByTagName('span')[1].innerHTML = 'Minimum 3 characters';
+        document.getElementsByClassName('logInMessage')[1].innerHTML = 'Minimum 3 characters';
+        y.setAttribute("style", "border: 1px solid #FF8A80");
     } else {
-        document.getElementsByTagName('span')[1].innerHTML = '';
+        document.getElementsByClassName('logInMessage')[1].innerHTML = '';
+        y.setAttribute("style", "border: 1px solid #ddd");
     }
 }
--- scripts/proiect_js_galerie.js
+++ scripts/proiect_js_galerie.js
@@ -1,16 +1,5 @@
 $(document).ready(function () {
-
-	// galeria este declansata de click pe div
-	/*$('#galerie').click(function(){
-		$('#galerie img:eq(1)').animate({
-			'left': 0
-		}, 500, function (){
-			$('#galerie img:eq(0)').css({'left':'-400px'})
-			$('#galerie').append($('#galerie img:eq(0)'))
-		});
-	});*/
-
-	//galeria este declansata automat la fiecare 2 secunde
+	
 	var x = setInterval(function () {
 		$('#galerie img:eq(1)').animate({
 			'left': 0
