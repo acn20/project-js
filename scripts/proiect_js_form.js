5c3f2bca6f031c10a02516a1793ea4d6d6862588
Andreea <andreea.nisipeanu@gmail.com>
2019-01-14 23:28:40 +0200

    renamed variables

--- scripts/proiect_js_form.js
+++ scripts/proiect_js_form.js
@@ -1,28 +1,28 @@
-var x = document.getElementsByClassName('field')[0];
-var y = document.getElementsByClassName('field')[1];
+var mailInput = document.getElementsByClassName('field')[0];
+var passwordInput = document.getElementsByClassName('field')[1];
 
 function mail() {
     if (
-        x.value.indexOf('@') != x.value.lastIndexOf('@') ||
-        x.value.indexOf('@') < 2 ||
-        x.value.lastIndexOf('.') > x.value.length - 3 ||
-        x.value.lastIndexOf('.') <= x.value.indexOf('@') + 2
+        mailInput.value.indexOf('@') != mailInput.value.lastIndexOf('@') ||
+        mailInput.value.indexOf('@') < 2 ||
+        mailInput.value.lastIndexOf('.') > mailInput.value.length - 3 ||
+        mailInput.value.lastIndexOf('.') <= mailInput.value.indexOf('@') + 2
 
     ) {
         document.getElementsByClassName('logInMessage')[0].innerHTML = 'Invalid email address';
-        x.setAttribute("style", "border: 1px solid #FF8A80");
+        mailInput.setAttribute("style", "border: 1px solid #FF8A80");
     } else {
         document.getElementsByClassName('logInMessage')[0].innerHTML = '';
-        x.setAttribute("style", "border: 1px solid #ddd");
+        mailInput.setAttribute("style", "border: 1px solid #ddd");
     }
 }
 
 function pass() {
-    if (y.value.length < 3) {
+    if (passwordInput.value.length < 3) {
         document.getElementsByClassName('logInMessage')[1].innerHTML = 'Minimum 3 characters';
-        y.setAttribute("style", "border: 1px solid #FF8A80");
+        passwordInput.setAttribute("style", "border: 1px solid #FF8A80");
     } else {
         document.getElementsByClassName('logInMessage')[1].innerHTML = '';
-        y.setAttribute("style", "border: 1px solid #ddd");
+        passwordInput.setAttribute("style", "border: 1px solid #ddd");
     }
 }
