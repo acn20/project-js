221c55d5f08313b3488cb4e97285dc0000f43d5a
Andreea <andreea.nisipeanu@gmail.com>
2019-01-15 00:13:10 +0200

    cleanup gallery

--- scripts/proiect_js_galerie.js
+++ scripts/proiect_js_galerie.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
-	
-	var x = setInterval(function () {
-		$('#galerie img:eq(1)').animate({
-			'left': 0
-		}, 700, function () {
-			$('#galerie img:eq(0)').css({ 'left': '-400px' })
-			$('#galerie').append($('#galerie img:eq(0)'))
+
+	var galleryInterval = setInterval(function () {//functia set interval returneaza un id (numar), ca sa-i poti da clear
+		$('#galerie img:eq(1)').animate({ 'left': 0 }, 700, function () {
+			$('#galerie img:eq(0)').css({ 'left': '-400px' });
+			$('#galerie').append($('#galerie img:eq(0)'));
 		});
-	}, 2500)
+	}, 2500);
 
 	$('#galerie').click(function () {
-		clearInterval(x);
+		clearInterval(galleryInterval);
 	});
 
 });
